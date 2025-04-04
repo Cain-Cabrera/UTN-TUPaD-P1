b38c0451819381f2aa53183b3fc70e1e33c8ddfa
Cain-Cabrera <caincito.cabrera7@gmail.com>
2025-04-04 08:21:47 -0300

    subo tp3 con algunos arreglos

--- 03_Estructuras_Condicionales/Cain_Cabrera_TP.py
+++ 03_Estructuras_Condicionales/Cain_Cabrera_TP.py
@@ -136,7 +136,7 @@ lower() y title() de Python para convertir entre mayúsculas y minúsculas.
 """
 
 nombre = input("Ingrese su nombre: ")
-opcion = input(f"\n '1' --> Si quiere su nombre en mayúsculas \n '2' --> Si quiere su nombre en minúsculas \n '3' --> Si quiere su nombre con la primera letra mayúscula \n -->  ")
+opcion = input(f"\n '1' --> Si quiere su nombre en mayúsculas \n '2' --> Si quiere su nombre en minúsculas / \n '3' --> Si quiere su nombre con la primera letra mayúscula \n -->  ") 
 match opcion:
     case "1":
         nombre = nombre.lower()
